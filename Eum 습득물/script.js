dc7477ffe7de166590f8fe0edf7571f322e0b63c
yeondon <s25030@gsm.hs.kr>
2025-05-14 21:01:42 +0900

    input file

---
+++ Eum 습득물/script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+  document.getElementById("file-icon").addEventListener("click", function () {
+    document.getElementById("fileInput").click();
+  });
+});
