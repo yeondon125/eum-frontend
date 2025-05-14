6def4a0fbf14deb1f05411d9dffa11ebed1a378c
yeondon <s25030@gsm.hs.kr>
2025-05-14 23:46:29 +0900

    js

--- Eum 습득물/script.js
+++ Eum 습득물/script.js
@@ -3,3 +3,21 @@ document.addEventListener("DOMContentLoaded", function () {
     document.getElementById("fileInput").click();
   });
 });
+
+function checkInputs() {
+  const inputs = document.getElementsByClassName("input");
+
+  let allFilled = true;
+
+  for (let i = 0; i < inputs.length; i++) {
+    if (inputs[i].value.trim() === "") {
+      allFilled = false;
+      break;
+    }
+  }
+
+  if (allFilled) {
+    document.getElementById("btn").
+  } 
+}
+
