9b95bdd64bcf894853529158800a7bbd299ef7f1
yeondon <s25030@gsm.hs.kr>
2025-05-15 11:10:20 +0900

    js 수정중

--- Eum 습득물/script.js
+++ Eum 습득물/script.js
@@ -1,8 +1,18 @@
 document.addEventListener("DOMContentLoaded", function () {
+  //포토를 클릭시 파일을 선택할 수 있도록 설정
   document.getElementById("photo").addEventListener("click", function () {
     document.getElementById("fileInput").click();
   });
+  //버튼 택스트 클릭시 함수호출출
+  document.getElementById("btn-active").addEventListener("click", function () {
+    document.getElementById("start").click();
+  });
+  document
+    .getElementById("start")
+    .click()
+    .addEventListener("click", function () {});
 
+  //input값 검사와 버튼 디자인 변경경
   const inputs = document.getElementsByClassName("input");
   const btn = document.getElementById("btn");
 
