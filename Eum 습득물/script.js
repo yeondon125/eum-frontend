16920fcbbb2422f160ace7f736255fe05c0e7690
yeondon <s25030@gsm.hs.kr>
2025-05-19 05:01:49 +0900

    js 수정중

--- Eum 습득물/script.js
+++ Eum 습득물/script.js
@@ -1,9 +1,36 @@
 document.addEventListener("DOMContentLoaded", function () {
-  // 포토 클릭 시 파일 선택
+  // 포토 클릭 시 파일 선택 {
   document.getElementById("photo").addEventListener("click", function () {
     document.getElementById("fileInput").click();
+    //}
+    //이미지 미리보기 {
+
+    document
+      .getElementById("fileInput")
+      .addEventListener("change", function (e) {
+        const file = e.target.files[0];
+        const preview = document.getElementById("preview");
+        const btn = document.getElementById("btn");
+
+        if (file) {
+          const reader = new FileReader();
+          reader.onload = function (event) {
+            preview.src = event.target.result;
+            preview.style.display = "block";
+
+            // btn.classList.remove("btn");
+            // btn.classList.add("btn-active");
+          };
+          reader.readAsDataURL(file);
+        } /*else {
+          btn.classList.remove("btn-active");
+          btn.classList.add("btn");
+        }*/
+      });
+    //}
   });
 
+  //글자 수 제한
   const input1 = document.querySelector(".lostname input"); // 습득물명 input
   const input2 = document.querySelector(".explanation textarea"); // 설명 textarea
   const max1 = document.getElementById("max1");
@@ -15,6 +42,7 @@ document.addEventListener("DOMContentLoaded", function () {
       max1.classList.remove("max-on");
       max1.classList.add("max");
       input1.classList.add("input-active");
+      input1.value = input1.value.trim().slice(0, 32);
     } else {
       max1.classList.remove("max");
       max1.classList.add("max-on");
@@ -25,6 +53,7 @@ document.addEventListener("DOMContentLoaded", function () {
     if (input2.value.trim().length > 192) {
       max2.classList.remove("max-on");
       max2.classList.add("max");
+      input2.value = input2.value.trim().slice(0, 192);
 
       input2.classList.add("input-active");
     } else {
@@ -34,18 +63,15 @@ document.addEventListener("DOMContentLoaded", function () {
       input2.classList.remove("input-active");
     }
   }
+  //인풋 입력 인지시 함수실행
   input1.addEventListener("input", checkInputs);
   input2.addEventListener("input", checkInputs);
 });
 
 ////버튼 색 바꾸기기
-//const btn = document.getElementById("btn");
+//
 //   if (allFilled) {
-//     btn.classList.remove("btn");
-//     btn.classList.add("btn-active");
-//   } else {
-//     btn.classList.remove("btn-active");
-//     btn.classList.add("btn");
+//
 //   }
 // }
 
