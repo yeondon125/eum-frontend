14bb708efce9a03a8f9aa207d1b9e8f272fb6822
yeondon <s25030@gsm.hs.kr>
2025-06-05 17:16:43 +0900

    이름 수정 완료

--- main화면/main화면(로그인O)/main-name.js
+++ main화면/main화면(로그인O)/main-name.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
   const name = document.getElementById("user-button");
-  const uname = localStorage.setItem("student_name");
+  const uname = localStorage.getItem("student_name");
   name.innerHTML = `<img src="../icons/Vector.svg" alt="사용자 아이콘" />${uname}`;
 });
