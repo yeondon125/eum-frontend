d711e1d90181d3c9810c84aba83ced76d1d95ba0
yeondon <s25030@gsm.hs.kr>
2025-06-05 19:15:28 +0900

    api 일부 수정

--- main화면/main화면(로그인O)/main-get.js
+++ main화면/main화면(로그인O)/main-get.js
@@ -1,3 +1,55 @@
+fetch("")
+  .then((response) => {
+    console.log("응답 상태:", response.status);
+    return response.json();
+  })
+  .then((data) => {
+    console.log("받은 데이터:", data);
+
+    // localStorage.setItem("posts", JSON.stringify(data));
+
+    const lostCard = document.getElementById("found-row");
+    if (!lostCard) {
+      console.error(" 'found' 클래스를 가진 요소가 없습니다.");
+      return;
+    }
+
+    lostCard.innerHTML = "";
+
+    data.getitem.content.forEach((post) => {
+      const id = post.boardId;
+      const card = document.createElement("div");
+      card.className = "card";
+      // console.log(`안녕하세요.${id}`);
+      card.id = id;
+      card.innerHTML = `
+        <p class="label">습득물</p>
+        <img
+          src="${post.getitem_url_image || "https://via.placeholder.com/150"}"
+          alt="습득물 이미지"
+          class="item-img"
+        />
+        <div class="info">
+          <p class="student"><img src="../icons/Vector.svg" alt="아이콘" /> ${
+            post.student_id || "학번없음"
+          }|${post.student_name || "이름없음"}</p>
+          <p class="name">${post.getitem_name || "제목 없음"}</p>
+        </div>`;
+
+      card.addEventListener("click", () => {
+        window.location.href = `detail.html?id=${id}`;
+      });
+      lostCard.appendChild(card);
+    });
+  })
+  .catch((err) => {
+    console.error("에러 발생:", err);
+    const lostCard = document.getElementsByClassName("foun-row");
+    if (lostCard) {
+      lostCard.innerHTML = "<p>게시글을 불러오지 못했습니다.</p>";
+    }
+  });
+
 // fetch("https://amond-blog.n-e.kr/api/v1/articles")
 //   .then((response) => {
 //     console.log("응답 상태:", response.status);
@@ -17,29 +69,28 @@
 //     lostCard.innerHTML = "";
 
 //     data.forEach((post) => {
-//       const id = content.boardId;
+//       const id = post.id;
 //       const card = document.createElement("div");
 //       card.className = "card";
-//       // console.log(`안녕하세요.${id}`);
 //       card.id = id;
+//       // console.log(`안녕하세요.${id}`);
 //       card.innerHTML = `
-//         <p class="label">분실물</p>
+//         <p class="label">습득물</p>
 //         <img
-//           src="${getitem_url_image || "https://via.placeholder.com/150"}"
-//           alt="분실물 이미지"
+//           src="${post.imageUrl || "https://via.placeholder.com/150"}"
+//           alt="습득물 이미지"
 //           class="item-img"
 //         />
 //         <div class="info">
 //           <p class="student"><img src="../icons/Vector.svg" alt="아이콘" /> ${
-//             student_id || "학번없음"
-//           }|${student_name || "이름없음"}</p>
-//           <p class="name">${"getitem_name" || "제목 없음"}</p>
+//             post.student || "학번|이름 없음"
+//           }</p>
+//           <p class="name">${post.title || "제목 없음"}</p>
 //         </div>
-//card.addEventListener("click", () => {
-//  window.location.href = `detail.html?id=${id}`;
-//});
-//lostCard.appendChild(card);
 //       `;
+//       card.addEventListener("click", () => {
+//         window.location.href = `detail.html?id=${id}`;
+//       });
 //       lostCard.appendChild(card);
 //     });
 //   })
@@ -50,55 +101,3 @@
 //       lostCard.innerHTML = "<p>게시글을 불러오지 못했습니다.</p>";
 //     }
 //   });
-
-fetch("https://amond-blog.n-e.kr/api/v1/articles")
-  .then((response) => {
-    console.log("응답 상태:", response.status);
-    return response.json();
-  })
-  .then((data) => {
-    console.log("받은 데이터:", data);
-
-    localStorage.setItem("posts", JSON.stringify(data));
-
-    const lostCard = document.getElementById("found-row");
-    if (!lostCard) {
-      console.error(" 'found' 클래스를 가진 요소가 없습니다.");
-      return;
-    }
-
-    lostCard.innerHTML = "";
-
-    data.forEach((post) => {
-      const id = post.id;
-      const card = document.createElement("div");
-      card.className = "card";
-      card.id = id;
-      // console.log(`안녕하세요.${id}`);
-      card.innerHTML = `
-        <p class="label">습득물</p>
-        <img
-          src="${post.imageUrl || "https://via.placeholder.com/150"}"
-          alt="습득물 이미지"
-          class="item-img"
-        />
-        <div class="info">
-          <p class="student"><img src="../icons/Vector.svg" alt="아이콘" /> ${
-            post.student || "학번|이름 없음"
-          }</p>
-          <p class="name">${post.title || "제목 없음"}</p>
-        </div>
-      `;
-      card.addEventListener("click", () => {
-        window.location.href = `detail.html?id=${id}`;
-      });
-      lostCard.appendChild(card);
-    });
-  })
-  .catch((err) => {
-    console.error("에러 발생:", err);
-    const lostCard = document.getElementsByClassName("lost")[0];
-    if (lostCard) {
-      lostCard.innerHTML = "<p>게시글을 불러오지 못했습니다.</p>";
-    }
-  });
