0933282e426816fff5442ae116c347ae47560632
yeondon <s25030@gsm.hs.kr>
2025-06-19 17:28:55 +0900

    회원가입 입

--- js/join2.js
+++ js/join2.js
@@ -82,28 +82,22 @@ document.addEventListener("DOMContentLoaded", function () {
         }),
       });
 
-      if (!response.ok) {
-        throw new Error("회원가입 실패: " + response.status);
-      }
+      // if (!response.ok) {
+      //   throw new Error("회원가입 실패: " + response.status);
+      // }
 
-      const text = await response.text();
-      let result = {};
+      // const text = await response.text();
+      // let result = {};
 
-      if (text.trim().startsWith("{")) {
-        try {
-          result = JSON.parse(text);
-        } catch (e) {
-          console.warn("⚠ JSON 파싱 실패:", text);
-        }
-      } else {
-        console.warn("⚠ 서버가 JSON 아닌 문자열을 반환:", text);
-      }
-
-      const token = result.token;
-      if (!token) {
-        alert("JWT 토큰이 응답에 없습니다.");
-        return;
-      }
+      // if (text.trim().startsWith("{")) {
+      //   try {
+      //     result = JSON.parse(text);
+      //   } catch (e) {
+      //     console.warn("⚠ JSON 파싱 실패:", text);
+      //   }
+      // } else {
+      //   console.warn("⚠ 서버가 JSON 아닌 문자열을 반환:", text);
+      // }
 
       const payload = JSON.parse(atob(token.split(".")[1]));
       localStorage.setItem("token", token);
@@ -111,7 +105,7 @@ document.addEventListener("DOMContentLoaded", function () {
       localStorage.setItem("student_id", payload.sub);
 
       alert("회원가입 성공!");
-      window.location.href = "http://127.0.0.1:5500/Eum로그인/login.html";
+      window.location.href = "https://eum-frontend.vercel.app/";
     } catch (error) {
       console.error("회원가입 실패:", error);
       alert("서버 요청 중 오류가 발생했습니다. 다시 시도해주세요");
