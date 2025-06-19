4ae7be57a0a00f13b370d7979db237254fb3d08a
yeondon <s25030@gsm.hs.kr>
2025-06-19 23:47:40 +0900

    회원가입 수정중

--- js/join2.js
+++ js/join2.js
@@ -19,13 +19,15 @@ submitButton.addEventListener("click", async () => {
     });
 
     // 상태코드 확인만 남기고, 응답 파싱 제거
-    if (!response.ok) {
-      throw new Error("회원가입 실패: " + response.status);
+    if (!response.ok && response.status == 402) {
+      throw new Error(response.message);
+    } else {
+      throw new Error(response.message);
     }
 
     // ✅ 성공 처리
     alert("회원가입 성공!");
-    window.location.href = "http://127.0.0.1:5500/Eum로그인/login.html";
+    window.location.href = "https://eum-frontend.vercel.app/";
   } catch (error) {
     console.error("회원가입 실패:", error);
     alert("서버 요청 중 오류가 발생했습니다. 다시 시도해주세요");
